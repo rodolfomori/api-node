69b998ae2c1694e5ce2affef40e3840b3f9453d5
rodolfomori <rod_dolpho@hotmail.com>
2022-09-21 17:25:27 -0300

    aula 03 - finalizada

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+})
+
+export default mongoose.model('User', userSchema)
